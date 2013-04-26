2b82324abc5203fc3b84d561c2b72b1fe00d85f5
root <root@localhost.localdomain>
2013-04-26 14:20:07 -0700

    Added Net::RabbitMQ version for comparison

---
+++ implementations/Net-RabbitMQ/QueueTest.pm
@@ -0,0 +1,77 @@
+package Queue {
+    use 5.014; # implies strict
+    use warnings;
+    use utf8;
+    use Net::RabbitMQ;
+    use DBI;
+    use JSON;
+
+    my $channel      = 1;
+    my $queue_name  = 'queue_test';
+    sub broker {
+        my $class = shift;
+        my $self = bless {}, $class;
+        $self->{channel}    ||= $channel;
+        $self->{queue_name} ||= $queue_name;
+        $self->{nrmq} = Net::RabbitMQ->new;
+        $self->{nrmq}->connect("localhost", { user => "guest", password => "guest" });
+        $self->{nrmq}->channel_open($self->{channel});
+        $self->{nrmq}->queue_declare($self->{channel}, $self->{queue_name});
+        return $self;
+    }
+    sub message_queue {
+        my ($self,$payload) = @_;
+        $payload = encode_json( $payload );
+        $self->{nrmq}->publish($self->{channel}, $self->{queue_name}, $payload)
+    }
+    sub message_dequeue {
+        my $self        = shift;
+        my $wanted_msgs = shift || 1;
+        my @messages;
+        # Look into ->basic_qos and prefetch
+        for (1 .. $wanted_msgs) {
+            my $amqp = $self->{nrmq}->get(
+                $self->{channel}, $self->{queue_name}, { no_ack => 0 },
+            );
+            next unless $amqp;
+            push @messages, Message->new($amqp, $self);
+        }
+        return @messages;
+    }
+    sub message_count {
+        my $self = shift;
+        my ($name, $jobs, $consumer ) = $self->{nrmq}->queue_declare($self->{channel}, $self->{queue_name});
+        return $jobs;
+    }
+    sub message_accept {
+        my ($self,$id) = @_;
+        die 'No message id passed' unless defined $id;
+        $self->{nrmq}->ack($self->{channel}, $id);
+        return 1;
+    }
+    sub message_reject {
+        my ($self,$id) = @_;
+        die 'No message id passed' unless defined $id;
+        $self->{nrmq}->reject($self->{channel}, $id, 1);
+        return 1;
+    }
+}
+
+package Message {
+    use 5.014; # implies strict
+    use warnings;
+    use utf8;
+    use JSON;
+
+    sub new {
+        $_[1]{_queue} = $_[2] || Queue->new;
+        bless $_[1], $_[0];
+    };
+    sub id { unpack 'I', $_[0]{delivery_tag} }
+    sub payload { $_[0]{body} }
+    sub message { decode_json( $_[0]{body} ) }
+    sub message_accept { $_[0]->{_queue}->message_accept($_[0]->{delivery_tag}) }
+    sub message_reject { $_[0]->{_queue}->message_reject($_[0]->{delivery_tag}) }
+};
+
+1;
